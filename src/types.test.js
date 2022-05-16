210c67c2884ca30d2fb676452d9da29b26615520
Maxim Terzhevik <maxt2000@mail.ru>
2022-05-16 23:17:05 +0300

    add prettier

--- src/types.test.js
+++ src/types.test.js
@@ -1,19 +1,19 @@
-import { sum, multiplication, sumSymbolsLen } from './types';
+import { sum, multiplication, sumSymbolsLen } from "./types";
 
-describe('sum', () => {
-  it('sum number', () => {
+describe("sum", () => {
+  it("sum number", () => {
     expect(sum(2, 3)).toEqual(5);
   });
 });
 
-describe('multiplication', () => {
-  it('multiplication number', () => {
+describe("multiplication", () => {
+  it("multiplication number", () => {
     expect(multiplication(2, 3)).toEqual(6);
   });
 });
 
-describe('sumSymbolsLen', () => {
-  it('sumSymbolsLen number', () => {
-    expect(sumSymbolsLen('John', 'Lennon')).toEqual(10);
+describe("sumSymbolsLen", () => {
+  it("sumSymbolsLen number", () => {
+    expect(sumSymbolsLen("John", "Lennon")).toEqual(10);
   });
 });
