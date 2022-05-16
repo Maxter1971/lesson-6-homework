616e14ca7d47b57215fa8f4a52941057d03453cb
Maxim Terzhevik <maxt2000@mail.ru>
2022-05-16 19:49:29 +0300

    first test and env

---
+++ src/types.js
@@ -0,0 +1,9 @@
+export function sum(a, b) {
+  return a + b;
+}
+export function multiplication(a, b) {
+  return a * b;
+}
+export function sumSymbolsLen(first, second) {
+  return first.length + second.length;
+}
---
+++ src/types.test.js
@@ -0,0 +1,19 @@
+import { sum, multiplication, sumSymbolsLen } from './types';
+
+describe('sum', () => {
+  it('sum number', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+});
+
+describe('multiplication', () => {
+  it('multiplication number', () => {
+    expect(multiplication(2, 3)).toEqual(6);
+  });
+});
+
+describe('sumSymbolsLen', () => {
+  it('sumSymbolsLen number', () => {
+    expect(sumSymbolsLen('John', 'Lennon')).toEqual(10);
+  });
+});
